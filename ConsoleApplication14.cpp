4a143d84432d1d93185b05565db04c8dcb3800f4
Gleb <Gleb@DESKTOP-M9TRRQ2>
2024-04-06 13:43:47 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication14.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream> 
+using namespace std; 
+
+bool isPrime(int num) 
+{ 
+	if (num <= 1)
+{ 
+	return false; 
+} 
+for (int i = 2; i * i <= num; i++)
+{ if (num % i == 0) 
+{ 
+	return false; 
+} 
+} 
+return true; 
+}
+
+
+int main()
+{
+	isPrime(1);
+}
